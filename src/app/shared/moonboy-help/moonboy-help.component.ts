fe66149c6a31e9c075dcdc85a6ce10bd2f330c31
Saber Bjeoui <saber@atayen.us>
2022-02-23 09:28:11 +0100

    generate file for new component

---
+++ src/app/shared/moonboy-help/moonboy-help.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moonboy-help',
+  templateUrl: './moonboy-help.component.html',
+  styleUrls: ['./moonboy-help.component.scss']
+})
+export class MoonboyHelpComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
