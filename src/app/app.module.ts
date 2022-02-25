8f35334a7325ff2ace391a04498e5c70f5a6f93e
Mariem-Jebali <mariem.jebali2018@gmail.com>
2022-02-25 17:29:52 +0100

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '@environments/environment';
-import { getCurrencySymbol } from '@angular/common';
 import { HelpComponent } from './components/help/help.component';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 @NgModule({
