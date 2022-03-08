d27c5d2b4be7f92adad30275509ab6e6e7e18775
Nizar <nizar@atayen.us>
2022-03-08 17:59:02 +0100

    FX: Wrapping layout init functions with isplatformBrowser

--- src/app/wallet/wallet-routing.module.ts
+++ src/app/wallet/wallet-routing.module.ts
@@ -16,8 +16,10 @@ import { CryptoInfoComponent } from '@wallet/components/crypto-info/crypto-info.
 const routes: Routes = [
   {
     path: '',
-    component: WalletComponent,
+    component: WalletComponent
+/*
     canActivate: [AuthGuardService, PassphraseCheckedGuard]
+*/
   },
   {
     path: 'add-token',
