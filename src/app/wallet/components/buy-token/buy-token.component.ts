a3bf9241ccef86e331758f8a12be865f6214e8d6
fedisadouki <fedi.sadouki@gmail.com>
2022-03-10 11:09:56 +0100

    fix bugs back in buy

--- src/app/wallet/components/buy-token/buy-token.component.ts
+++ src/app/wallet/components/buy-token/buy-token.component.ts
@@ -544,8 +544,8 @@ export class BuyTokenComponent implements OnInit {
   }
   linstingBack(event: any) {
     if (event === true) {
-      this._location.back();
-      // this.router.navigate(['/wallet']);
+      // this._location.back();
+      this.router.navigate(['/wallet']);
     }
   }
   trackByCryptoMoneyValue(index: number, crypto: any): string {
