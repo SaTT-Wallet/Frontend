6c8f5b5717f90bc232017434c40ebd7190b05d2a
Nizar <nizar@atayen.us>
2022-03-16 17:09:31 +0100

    twitter test

--- src/twitter/twitter.component.ts
+++ src/twitter/twitter.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
+import { isPlatformServer } from '@angular/common';
 
 @Component({
   selector: 'app-twitter',
@@ -7,47 +8,52 @@ import { Meta } from '@angular/platform-browser';
   styleUrls: ['./twitter.component.scss']
 })
 export class TwitterComponent implements OnInit {
-  constructor(private meta: Meta) {}
+  constructor(
+    private meta: Meta,
+    @Inject(PLATFORM_ID) private platformId: string
+  ) {}
 
   ngOnInit(): void {
-    this.meta.addTag({
-      name: 'og:title',
-      content: 'twiiter test'
-    });
-    const imgMetaTag = this.meta.getTag(`name='og:image'`);
-    if (!imgMetaTag) {
+    if (isPlatformServer(this.platformId)) {
       this.meta.addTag({
-        name: 'og:image',
+        name: 'og:title',
+        content: 'twiiter test'
+      });
+      const imgMetaTag = this.meta.getTag(`name='og:image'`);
+      if (!imgMetaTag) {
+        this.meta.addTag({
+          name: 'og:image',
+          content: 'https://satt-token.com/assets/img/index/wallet.png'
+        });
+      }
+      this.meta.addTag({
+        name: 'og:image:type',
+        content: `website`
+      });
+      this.meta.addTag({
+        name: 'og:image:alt',
+        content: `campaign cover`
+      });
+      this.meta.addTag({
+        name: 'og:description',
+        content: 'twiiter'
+      });
+      this.meta.addTag({
+        name: 'og:type',
+        content: 'website'
+      });
+      this.meta.addTag({
+        name: 'og:url',
+        content: ``
+      });
+      this.meta.addTag({
+        name: 'twitter:card',
+        content: 'summary_large_image'
+      });
+      this.meta.addTag({
+        name: 'twitter:image',
         content: 'https://satt-token.com/assets/img/index/wallet.png'
       });
     }
-    this.meta.addTag({
-      name: 'og:image:type',
-      content: `website`
-    });
-    this.meta.addTag({
-      name: 'og:image:alt',
-      content: `campaign cover`
-    });
-    this.meta.addTag({
-      name: 'og:description',
-      content: 'twiiter'
-    });
-    this.meta.addTag({
-      name: 'og:type',
-      content: 'website'
-    });
-    this.meta.addTag({
-      name: 'og:url',
-      content: ``
-    });
-    this.meta.addTag({
-      name: 'twitter:card',
-      content: 'summary_large_image'
-    });
-    this.meta.addTag({
-      name: 'twitter:image',
-      content: 'https://satt-token.com/assets/img/index/wallet.png'
-    });
   }
 }
