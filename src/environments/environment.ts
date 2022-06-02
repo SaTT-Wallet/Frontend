2f3a886f5e32a0a030cb96530ef26a8d51de3b92
waelhanfi04 <wael@atayen.us>
2022-06-02 10:37:27 +0100

    fx

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,39 +4,39 @@
 
 export const environment = {
   production: false,
-  API_URL: 'https://api-preprod2.satt-token.com', //'http://localhost:3014',
-  // https://api-preprod.satt-token.com:3014
-  addresses: {
-    smartContracts: {
-      campaignERC20: '0xa5F46d6F4F3b318EeFF1B37e39491e52233c5975',
-      campaignBEP20: '0x2646cB509703B7c14aFC31aeEaF38b6c6A8bF478',
-      SATT_TOKENERC20: '0x2beF0d7531f0aaE08ADc26A0442Ba8D0516590d0',
-      SATT_TOKENBEP20: '0x6fAc729f346A46fC0093126f237b4A520c40eb89',
-      TETHER_TOKENERC20: '0x3B2243E3Aeb6d4A102061BCE6C5c233c2C08fF65',
-      DAI: '0x7d6550Bb3946c0BB0701c75baBE2f679E01F3f3E',
-      BUSD: '0x4CB4473Af06B844d06b5eDeF08983B2C5C61e5af'
-    }
-  },
-  domainName: 'https://dev.satt.atayen.us',
-  telegramBot: 'TestnetAtayenBot',
-  firebase: {
-    apiKey: 'AIzaSyAHfBKuneC6G0Uu9RKwI9hZKOjNiPQRd1U',
-    authDomain: 'satt-token.firebaseapp.com',
-    databaseURL: 'https://satt-token.firebaseio.com',
-    projectId: 'satt-token',
-    storageBucket: 'satt-token.appspot.com',
-    messagingSenderId: '284190209745',
-    appId: '1:284190209745:web:dc01085f6a6dabfcc837f0',
-    measurementId: 'G-DSJK01CZ0X'
-  },
-  fcmredirectUrl: 'http://localhost:4200/#/home',
-  bscan: 'https://testnet.bscscan.com/tx/',
-  etherscan: 'https://ropsten.etherscan.io/tx/',
-  bscanaddr: 'https://testnet.bscscan.com/address/',
-  etherscanaddr: 'https://ropsten.etherscan.io/address/',
-  simplexUrl: 'https://sandbox.test-simplexcc.com/payments/new'
+  // API_URL: 'https://api-preprod2.satt-token.com', //'http://localhost:3014',
+  // // https://api-preprod.satt-token.com:3014
+  // addresses: {
+  //   smartContracts: {
+  //     campaignERC20: '0xa5F46d6F4F3b318EeFF1B37e39491e52233c5975',
+  //     campaignBEP20: '0x2646cB509703B7c14aFC31aeEaF38b6c6A8bF478',
+  //     SATT_TOKENERC20: '0x2beF0d7531f0aaE08ADc26A0442Ba8D0516590d0',
+  //     SATT_TOKENBEP20: '0x6fAc729f346A46fC0093126f237b4A520c40eb89',
+  //     TETHER_TOKENERC20: '0x3B2243E3Aeb6d4A102061BCE6C5c233c2C08fF65',
+  //     DAI: '0x7d6550Bb3946c0BB0701c75baBE2f679E01F3f3E',
+  //     BUSD: '0x4CB4473Af06B844d06b5eDeF08983B2C5C61e5af'
+  //   }
+  // },
+  // domainName: 'https://dev.satt.atayen.us',
+  // telegramBot: 'TestnetAtayenBot',
+  // firebase: {
+  //   apiKey: 'AIzaSyAHfBKuneC6G0Uu9RKwI9hZKOjNiPQRd1U',
+  //   authDomain: 'satt-token.firebaseapp.com',
+  //   databaseURL: 'https://satt-token.firebaseio.com',
+  //   projectId: 'satt-token',
+  //   storageBucket: 'satt-token.appspot.com',
+  //   messagingSenderId: '284190209745',
+  //   appId: '1:284190209745:web:dc01085f6a6dabfcc837f0',
+  //   measurementId: 'G-DSJK01CZ0X'
+  // },
+  // fcmredirectUrl: 'http://localhost:4200/#/home',
+  // bscan: 'https://testnet.bscscan.com/tx/',
+  // etherscan: 'https://ropsten.etherscan.io/tx/',
+  // bscanaddr: 'https://testnet.bscscan.com/address/',
+  // etherscanaddr: 'https://ropsten.etherscan.io/address/',
+  // simplexUrl: 'https://sandbox.test-simplexcc.com/payments/new'
 
-  /*API_URL: 'https://api2.satt-token.com:3016',
+  API_URL: 'https://api2.satt-token.com:3016',
   addresses: {
     smartContracts: {
       campaignERC20: '0xf961c85517fe86244cb60889afc4a928068d49ef',
@@ -65,7 +65,7 @@ export const environment = {
   etherscan: 'https://etherscan.io/tx/',
   bscanaddr: 'https://bscscan.com/address/',
   etherscanaddr: 'https://etherscan.io/address/',
-  simplexUrl: 'https://checkout.simplexcc.com/payments/new'*/
+  simplexUrl: 'https://checkout.simplexcc.com/payments/new'
 };
 
 /*
