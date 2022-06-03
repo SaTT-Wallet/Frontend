c4687e3c79639bfddbfdc4cc1a4f2296cdf61368
Saber Bjeoui <saber@atayen.us>
2022-06-03 15:01:36 +0100

    Fix: open user tiktok profile in new page

--- src/app/user-settings/components/social-networks/social-networks.component.ts
+++ src/app/user-settings/components/social-networks/social-networks.component.ts
@@ -271,6 +271,8 @@ export class SocialNetworksComponent implements OnInit {
         window.open('https://www.instagram.com/' + userName, '_blank');
       } else if (network === 'linkedin') {
         window.open('https://www.linkedin.com/company/' + userName, '_blank');
+      } else if (network === 'tiktok') {
+        window.open('https://www.tiktok.com/@' + userName.replace(/\s/g, ''));
       }
     }
   }
