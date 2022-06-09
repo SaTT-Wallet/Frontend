20a93d57ac4d1e9efd83106d4dc11a64ee0c3d5d
Saber Bjeoui <saber@atayen.us>
2022-06-09 14:45:33 +0100

    Fix: update SaTT polygon SmartContract

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,17 +4,15 @@
 
 export const environment = {
   production: false,
-  /*
   API_URL: 'https://api-preprod2.satt-token.com',
-*/
   // 'https://api-preprod2.satt-token.com:3015',
   // API_URL: 'https://localhost:3015',
   //https://api-preprod.satt-token.com:3014
-  /* addresses: {
+  addresses: {
     smartContracts: {
       campaignERC20: '0xa5F46d6F4F3b318EeFF1B37e39491e52233c5975',
       campaignBEP20: '0xcef9b240c13f269354e5ae3d6808fecf9834bf10',
-      campaignPOLYGON: '0x8CC1e2E2C4a04c3029579fD9612FEBb0323C319F',
+      campaignPOLYGON: '0x19e0a7b387694ff10bC3203D2b4A1Cc3bb27f3e8',
       SATT_TOKENERC20: '0x2beF0d7531f0aaE08ADc26A0442Ba8D0516590d0',
       SATT_TOKENBEP20: '0x6fAc729f346A46fC0093126f237b4A520c40eb89',
       SATT_TOKENPOLYGON: '0x195DC8342D923D3dFe0167Dc902A33Eabd801653',
@@ -42,42 +40,42 @@ export const environment = {
   polygonscanAddr: 'https://mumbai.polygonscan.com/tx/',
   etherscanaddr: 'https://ropsten.etherscan.io/address/',
   simplexUrl: 'https://sandbox.test-simplexcc.com/payments/new',
-  gmtId: 'GTM-K2RMTJ3'*/
+  gmtId: 'GTM-K2RMTJ3'
 
-  API_URL: 'https://api2.satt-token.com:3016',
-  addresses: {
-    smartContracts: {
-      campaignERC20: '0xf961c85517fe86244cb60889afc4a928068d49ef',
-      campaignBEP20: '0xcef9b240c13f269354e5ae3d6808fecf9834bf10',
-      SATT_TOKENERC20: '0xdf49c9f599a0a9049d97cff34d0c30e468987389',
-      SATT_TOKENBEP20: '0x448bee2d93be708b54ee6353a7cc35c4933f1156',
-      TETHER_TOKENERC20: '0xdac17f958d2ee523a2206206994597c13d831ec7',
-      SATT_TOKENPOLYGON: '0x195DC8342D923D3dFe0167Dc902A33Eabd801653',
-      campaignPOLYGON: '0x8CC1e2E2C4a04c3029579fD9612FEBb0323C319F',
+  // API_URL: 'https://api2.satt-token.com:3016',
+  // addresses: {
+  //   smartContracts: {
+  //     campaignERC20: '0xf961c85517fe86244cb60889afc4a928068d49ef',
+  //     campaignBEP20: '0xcef9b240c13f269354e5ae3d6808fecf9834bf10',
+  //     SATT_TOKENERC20: '0xdf49c9f599a0a9049d97cff34d0c30e468987389',
+  //     SATT_TOKENBEP20: '0x448bee2d93be708b54ee6353a7cc35c4933f1156',
+  //     TETHER_TOKENERC20: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+  //     SATT_TOKENPOLYGON: '0x195DC8342D923D3dFe0167Dc902A33Eabd801653',
+  //     campaignPOLYGON: '0x8CC1e2E2C4a04c3029579fD9612FEBb0323C319F',
 
-      DAI: '0x6b175474e89094c44da98b954eedeac495271d0f',
-      BUSD: '0xe9e7cea3dedca5984780bafc599bd69add087d56'
-    }
-  },
-  domainName: 'https://satt.atayen.us',
-  telegramBot: 'v2_satt_token_bot',
-  firebase: {
-    apiKey: 'AIzaSyAHfBKuneC6G0Uu9RKwI9hZKOjNiPQRd1U',
-    authDomain: 'satt-token.firebaseapp.com',
-    //databaseURL: 'https://satt-token.firebaseio.com',
-    projectId: 'satt-token',
-    storageBucket: 'satt-token.appspot.com',
-    messagingSenderId: '284190209745',
-    appId: '1:284190209745:web:dc01085f6a6dabfcc837f0',
-    measurementId: 'G-DSJK01CZ0X'
-  },
-  fcmredirectUrl: 'https://satt.atayen.us/#/home',
-  bscan: 'https://bscscan.com/tx/',
-  etherscan: 'https://etherscan.io/tx/',
-  bscanaddr: 'https://bscscan.com/address/',
-  etherscanaddr: 'https://etherscan.io/address/',
-  simplexUrl: 'https://checkout.simplexcc.com/payments/new',
-  polygonscanAddr: 'https://mumbai.polygonscan.com/tx/'
+  //     DAI: '0x6b175474e89094c44da98b954eedeac495271d0f',
+  //     BUSD: '0xe9e7cea3dedca5984780bafc599bd69add087d56'
+  //   }
+  // },
+  // domainName: 'https://satt.atayen.us',
+  // telegramBot: 'v2_satt_token_bot',
+  // firebase: {
+  //   apiKey: 'AIzaSyAHfBKuneC6G0Uu9RKwI9hZKOjNiPQRd1U',
+  //   authDomain: 'satt-token.firebaseapp.com',
+  //   //databaseURL: 'https://satt-token.firebaseio.com',
+  //   projectId: 'satt-token',
+  //   storageBucket: 'satt-token.appspot.com',
+  //   messagingSenderId: '284190209745',
+  //   appId: '1:284190209745:web:dc01085f6a6dabfcc837f0',
+  //   measurementId: 'G-DSJK01CZ0X'
+  // },
+  // fcmredirectUrl: 'https://satt.atayen.us/#/home',
+  // bscan: 'https://bscscan.com/tx/',
+  // etherscan: 'https://etherscan.io/tx/',
+  // bscanaddr: 'https://bscscan.com/address/',
+  // etherscanaddr: 'https://etherscan.io/address/',
+  // simplexUrl: 'https://checkout.simplexcc.com/payments/new',
+  // polygonscanAddr: 'https://mumbai.polygonscan.com/tx/'
 };
 
 /*
