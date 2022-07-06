8e1344b99e0b5249b22f3cebad749155b9f56d9a
waelhanfi04 <wael@atayen.us>
2022-07-06 16:46:49 +0100

    environement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //API_URL: 'https://api-preprod2.satt-token.com',
-   API_URL: 'https://localhost:3015',
+  API_URL: 'https://api-preprod2.satt-token.com',
+  // API_URL: 'https://localhost:3015',
   //https://api-preprod.satt-token.com:3014
   addresses: {
     smartContracts: {
