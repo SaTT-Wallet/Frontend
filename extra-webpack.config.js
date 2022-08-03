2d7b1498a66772f53b6b03fa08258dd322b376e6
waelhanfi04 <wael@atayen.us>
2022-08-03 10:02:02 +0100

    change web

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,6 +1,6 @@
 console.log('custom webpack config');
 module.exports = {
   plugins: [
-    { test: /.(ts|js)$/, loader: 'regexp-replace-loader', query: { match: { pattern: '\[(Mouse|Keyboard)Event\]', flags: 'g' }, replaceWith: '[]', } }
+    { test: /.(ts|js)$/, rules: 'regexp-replace-loader', query: { match: { pattern: '\[(Mouse|Keyboard)Event\]', flags: 'g' }, replaceWith: '[]', } }
   ]
 };
