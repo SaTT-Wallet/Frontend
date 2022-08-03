0a38f479eab842b788954ca5ad4a86f0de905ab0
waelhanfi04 <wael@atayen.us>
2022-08-03 11:09:10 +0100

    change angular

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,6 +1,6 @@
 console.log('custom webpack config');
-module.exports = {
-  plugins: [
-    { test: /.(ts|js)$/, rules: 'regexp-replace-loader', query: { match: { pattern: '\[(Mouse|Keyboard)Event\]', flags: 'g' }, replaceWith: '[]', } }
-  ]
-};
+// module.exports = {
+//   plugins: [
+//     { test: /.(ts|js)$/, rules: 'regexp-replace-loader', query: { match: { pattern: '\[(Mouse|Keyboard)Event\]', flags: 'g' }, replaceWith: '[]', } }
+//   ]
+// };
