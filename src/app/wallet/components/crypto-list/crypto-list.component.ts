4034b79133ab572d6db627bfcc3d53deaa77e091
Saber Bjeoui <saber@atayen.us>
2022-08-10 12:01:34 +0100

    Fix: tweak sunswap url to auto select BTT when sunswap web app loads

--- src/app/wallet/components/crypto-list/crypto-list.component.ts
+++ src/app/wallet/components/crypto-list/crypto-list.component.ts
@@ -820,10 +820,15 @@ export class CryptoListComponent implements OnInit, OnDestroy {
               this.buy(crypto.undername);
             } else if (
               crypto.symbol === 'SATTPOLYGON' ||
-              crypto.symbol === 'MATIC' ||
-              crypto.symbol === 'BTT'
+              crypto.symbol === 'MATIC'
             ) {
               this.router.navigate(['/wallet']);
+            } else if (crypto.symbol === 'BTT') {
+              if (isPlatformBrowser(this.platformId))
+                window.open(
+                  'https://sunswap.com/#/v2?lang=en-US&t0=TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t&t1=TAFjULxiVgT4qWk6UZwjqwZXTSaGaqnVp4&type=swap',
+                  '_blank'
+                );
             } else {
               this.goToBuy(crypto.symbol, crypto.network);
             }
