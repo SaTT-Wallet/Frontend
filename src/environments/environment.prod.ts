01fec6699ffbdffa769bed656d498a6d60df6a2a
waelhanfi04 <wael@atayen.us>
2022-08-29 14:55:17 +0100

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -17,7 +17,7 @@ export const environment = {
       SATT_TOKENERC20: '0x2bef0d7531f0aae08adc26a0442ba8d0516590d0',
       SATT_TOKENBEP20: '0x6fAc729f346A46fC0093126f237b4A520c40eb89',
       SATT_TOKEN_TRON: 'TZ9mUjmxCtowj9h68NJq626qaGWiv61KWo',
-      SATT_TOKENPOLYGON: '0x195DC8342D923D3dFe0167Dc902A33Eabd801653',
+
       SATT_TOKENBTT: '0xaabda8813dF0fDc254B3aeD3d901E3838f0CCfCF',
       SATT_TOKENTRON: 'TZ9mUjmxCtowj9h68NJq626qaGWiv61KWo',
       TETHER_TOKENERC20: '0x3B2243E3Aeb6d4A102061BCE6C5c233c2C08fF65',
@@ -58,21 +58,22 @@ export const environment = {
   DEV-ENV
 */
   /*
-  API_URL: 'https://api-preprod2.satt-token.com',
-  // 'https://api-preprod2.satt-token.com:3015',
-  //'https://localhost:3015',
-  //https://api-preprod.satt-token.com:3014
+  API_URL: 'https://api2.satt-token.com'
   addresses: {
     smartContracts: {
-      campaignERC20: '0xa5F46d6F4F3b318EeFF1B37e39491e52233c5975',
+      campaignERC20: '0xf961c85517fe86244cb60889afc4a928068d49ef',
       campaignBEP20: '0xcef9b240c13f269354e5ae3d6808fecf9834bf10',
-      campaignPOLYGON: '0x8CC1e2E2C4a04c3029579fD9612FEBb0323C319F',
-      SATT_TOKENERC20: '0x2beF0d7531f0aaE08ADc26A0442Ba8D0516590d0',
+      campaignPOLYGON: '0xa01f80042512Cdf9355a66CBB1266240c0456513',
+      campaignBTT: '0xa01f80042512Cdf9355a66CBB1266240c0456513',
+      campaignTRON: 'TPeLf9WLzUD8tUoc7dtfGVqPUrnKGip2PN',
+
+
+
+      SATT_TOKENERC20: '0xdf49c9f599a0a9049d97cff34d0c30e468987389',
       SATT_TOKENBEP20: '0x6fAc729f346A46fC0093126f237b4A520c40eb89',
-      SATT_TOKENPOLYGON: '0x195DC8342D923D3dFe0167Dc902A33Eabd801653',
-      TETHER_TOKENERC20: '0x3B2243E3Aeb6d4A102061BCE6C5c233c2C08fF65',
-      DAI: '0x7d6550Bb3946c0BB0701c75baBE2f679E01F3f3E',
-      BUSD: '0x4CB4473Af06B844d06b5eDeF08983B2C5C61e5af'
+      TETHER_TOKENERC20: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+      DAI: '0x6b175474e89094c44da98b954eedeac495271d0f',
+      BUSD: '0xe9e7cea3dedca5984780bafc599bd69add087d56'
     }
   },
   domainName: 'https://dev.satt.atayen.us',
