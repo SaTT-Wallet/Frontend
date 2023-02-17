f042a70ebc9f235b9b8abf1cf67d0c9569c1d4cb
hichri-louay <louay@satt.com>
2023-02-17 17:07:15 +0100

    fix scroll and change systeme of migration modal

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -80,6 +80,10 @@ export class WalletComponent implements OnInit, OnDestroy {
   private tronWalletCreatedSuccessModal!: TemplateRef<any>;
 
 
+  @ViewChild('migration', { static: false })
+  private migration!: TemplateRef<any>;
+
+
 
 
   
@@ -771,9 +775,14 @@ export class WalletComponent implements OnInit, OnDestroy {
     ctx?.fillRect(20, 20, 150, 100);
     if (this.hasWalletV2) this.verifyOnBoarding();
     this.getDetails();
+    console.log({migrate: this.migrate})
+    console.log({wallet: this.hasWalletV2})
+    console.log({show: this.show})
+    
   }
   migrateButton() : void {
     this.migrate = "open"
+    this.openModal(this.migration);
    
     
   }
@@ -833,6 +842,7 @@ export class WalletComponent implements OnInit, OnDestroy {
             response?.data?.tronAddress
           ) {
             this.closeModal(this.createWalletV2Modal);
+            this.openModal(this.migration);
           } else {
             //wrong
             //this.closeModal(this.createWalletV2Modal)
@@ -843,7 +853,9 @@ export class WalletComponent implements OnInit, OnDestroy {
   getMigrationStatus($event: any) {
     this.tokenStorageService.setModaleMigrate($event);
     this.migrate = $event;
-    
+    if($event == "close") {
+      this.closeModal(this.migration);
+    }
     this.versionText =
       this.tokenStorageService.getWalletVersion() === 'v2'
         ? 'Old Wallet'
@@ -965,7 +977,9 @@ export class WalletComponent implements OnInit, OnDestroy {
           localStorage.getItem('wallet_version') === 'v1'
             ? true
             : false;
-
+        if(this.show && this.hasWalletV2 && this.migrate === "open") {
+          this.openModal(this.migration)
+        }
         if (this.show === false) {
           this.height = '250px';
         }
@@ -1118,6 +1132,7 @@ export class WalletComponent implements OnInit, OnDestroy {
               keyboard: false
             });
           } else {
+            
             this.hasWalletV2 = true;
             localStorage.setItem('existV2', 'true');
           }
