a6b3c6e62e645b8542010c286200e7cc907a183a
rania <rania.morheg@atayen.us>
2023-04-05 07:24:42 +0100

    draft

--- src/app/src/app/campaigns/components/roi-modal/roi-modal.component.ts
+++ src/app/src/app/campaigns/components/roi-modal/roi-modal.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CampaignHttpApiService } from '@app/core/services/campaign/campaign.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-roi-modal',
@@ -6,14 +10,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./roi-modal.component.scss']
 })
 export class RoiModalComponent implements OnInit {
+  campaignId: string = '';
+  tokenName: string = '';
+  campaignId: string = '';
+  isDestroyedSubject = new Subject();
   // @ViewChild('modal') modal: ElementRef;
   // @Input() title: string;
 
-  constructor() { }
+  constructor() { 
 
-  ngOnInit(): void {
+    public CampaignService: CampaignHttpApiService,
+  
+    private ActivatedRoute: ActivatedRoute
   }
 
+  ngOnInit(): void {
+    this.ActivatedRoute.params
+    .pipe(
+      mergeMap((params) => {
+        this.campaignId = params['campaign_id'];
+        return this.CampaignService.getOneById(this.campaignId,'projection');
+      }),
+      takeUntil(this.isDestroyedSubject)
+    )
+    .subscribe((data: any) => {
+      this.campaigndata = data.data;
+      this.tokenName = data.data.token.name
+      let performance = this.campaigndata.ratios[0]?.oracle;
+      if (performance?.length > 1 && performance === 'twitter') {
+        this.ratioLink = true;
+      }
+      this.parentFunction(this.networkWallet).subscribe();
+    });
+  }
+// getCampaignData(){
+//   this.CampaignService.getOneById(this.campaignId)
+//       .pipe(
+//         takeUntil(this.isDestroyed$),
+//         map((res: any) => res.data)
+//       )
+//       .subscribe((data: any) => {
+     
+//       });
+// }
   // openModal() {
   //   this.modal.nativeElement.classList.add('show');
   //   document.body.classList.add('modal-open');
