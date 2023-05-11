1776040f75cd8ec1cca677a1077d11f023505f1c
hichri-louay <124356992+hichri-louay@users.noreply.github.com>
2023-05-11 12:52:01 +0200

    Estimation fees pools (#274)
    * estimation fees wip
    
    * add fees for creation campaign ( bep20 && erc20 )
    
    * wip fees calcul in harvest section
    
    * add fees estimation in harvest section
    
    * remove logs
    
    * add polygon fees
    
    * add btt network fees

--- src/app/campaigns/components/farm-post-card/farm-post-card.component.ts
+++ src/app/campaigns/components/farm-post-card/farm-post-card.component.ts
@@ -14,7 +14,7 @@ import { EButtonActions } from '@app/core/enums';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Participation } from '@app/models/participation.model';
 import { NotificationService } from '@core/services/notification/notification.service';
-import { filter, map, switchMap, takeUntil } from 'rxjs/operators';
+import { catchError, filter, map, switchMap, takeUntil } from 'rxjs/operators';
 import { CampaignHttpApiService } from '@core/services/campaign/campaign.service';
 import { ParticipationListStoreService } from '@campaigns/services/participation-list-store.service';
 import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
@@ -183,12 +183,31 @@ export class FarmPostCardComponent implements OnInit {
       action: EButtonActions.GET_MY_GAINS
     });
 
-    this.router.navigate(
-      [`/home/campaign/${prom.campaign._id}/recover-my-gains`],
-      {
-        queryParams: { prom_hash: prom.hash, id: prom.campaign._id }
+
+    /************   FETCH NETWORK OF CAMPAIGN     ***********/
+    
+    let network = "";
+    this.campaignService.getOneById(prom.campaign._id)
+    .subscribe(
+      (res) => {
+      network = res.data.token.type;
+      this.router.navigate(
+        [`/home/campaign/${prom.campaign._id}/recover-my-gains`],
+        {
+          queryParams: {prom_hash: prom.hash, id: prom.campaign._id, network: network}
+        }
+      );
+    },
+      (err:any) => {
+        this.router.navigate(
+          [`/home/campaign/${prom.campaign._id}/recover-my-gains`],
+          {
+            queryParams: {prom_hash: prom.hash, id: prom.campaign._id}
+          }
+        );
       }
-    );
+    )
+    
   }
   validateLink(prom: any) {
     this.blockchainActions.onActionButtonClick({
