b86ed98b794e20b9726154ae38eaee119a578e9b
louay-hichri <louay@satt.com>
2023-05-15 11:39:03 +0200

    remove comments

--- src/app/social-accounts/components/header-social-registartion/header-social-registartion.component.ts
+++ src/app/social-accounts/components/header-social-registartion/header-social-registartion.component.ts
@@ -78,49 +78,6 @@ export class HeaderSocialRegistartionComponent implements OnInit {
     this.socialAccountFacadeService.dispatchLogoutSocialAccounts();
     this.accountFacadeService.dispatchLogoutAccount();
     this.router.navigate(['/auth/login']);
-    
-    /*this.tokenStorageService.logout().subscribe(
-      () => {
-        
-        this.campaignDataStore.clearDataStore(); // clear globale state before logging out user.
-        
-
-        //clear totalBalance and cryptoList
-        this.accountFacadeService.dispatchLogoutAccount(); //clear account user
-        this.socialAccountFacadeService.dispatchLogoutSocialAccounts(); // clear social accounts
-        
-        this.router.navigate(['/auth/login']);
-        
-      }
-      // () => {
-      //   this.campaignFacade.clearLinksListStore();
-      //   this.campaignDataStore.clearDataStore(); // clear globale state before logging out user.
-      //   this.ParticipationListStoreService.clearDataFarming();
-      //   this.walletFacade.dispatchLogout(); //clear totalBalance and cryptoList
-      //   this.accountFacadeService.dispatchLogoutAccount(); //clear account user
-      //   this.socialAccountFacadeService.dispatchLogoutSocialAccounts(); // clear social accounts
-      //   this.ParticipationListStoreService.nextPage.pageNumber = 0;
-      //   this.profileSettingsFacade.clearProfilePicStore();
-      //   this.authStoreService.clearStore();
-      //   this.tokenStorageService.clear();
-      //   this.kycFacadeService.dispatchLogoutKyc();
-      //   this.isConnected = false;
-      //   this.authService.setIsAuthenticated(false);
-      //   if (isPlatformBrowser(this.platformId)) {
-      //     window.location.reload();
-      //   }
-      //   this.router.navigate(['/auth/login']);
-      // }
-    );*/
-    /*this.campaignDataStore.clearDataStore(); // clear globale state before logging out user.
-    this.tokenStorageService.signOut();
-    this.socialAccountFacadeService.dispatchLogoutSocialAccounts();
-    this.accountFacadeService.dispatchLogoutAccount();
-    // if (isPlatformBrowser(this.platformId)) {
-    //   window.location.reload();
-    // }
-    //window.location.assign("https://satt.atayen.us/#/")
-    this.router.navigate(['/auth/login']);*/
   }
 
   refresh() {
