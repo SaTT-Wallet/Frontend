056721d779d42e2a816d7458680a94178e8c5c28
hichri-louay <louay@satt.com>
2023-05-24 16:33:06 +0100

    refactoring showNumbersRules pipe

--- src/app/shared/pipes/showNumbersRule.ts
+++ src/app/shared/pipes/showNumbersRule.ts
@@ -25,39 +25,26 @@ export class ShowNumbersRule implements PipeTransform {
    * @param value
    */
   transform(value: string, modeCryptoList?: boolean): string {
-    if (value) {
-      const bigValue = isNaN(+value) ? new Big(0) : new Big(value);
-      if (bigValue.div(1).toNumber() === 0) {
-        return '0';
-      } else {
-        const numberRules = [
-          { range: [0, 0.1], precision: 8 },
-          { range: [0.1, 1.9], precision: 6 },
-          { range: [2, 9.9999], precision: 4 },
-          { range: [10, 9999.99], precision: 2 },
-          { range: [10000, Infinity], precision: 2 }
-        ];
-        for (const rule of numberRules) {
-          if(rule.range[0] === 10000) {
-            if(bigValue.gte(rule.range[0])) {
-              if(modeCryptoList) return this.handleFixedValue(bigValue, rule.precision)
-              return this.handleFormattedValue(bigValue, '0.0-2')
-            }
-          } else {
-            if(bigValue.gte(rule.range[0]) && bigValue.lte(rule.range[1])) {
-              if(rule.range[0] === 10) {
-                if(modeCryptoList) {
-                  return this.handleFixedValue(bigValue, rule.precision)
-                } else return this.handleFormattedValue(bigValue, '0.0-2')
-              } else {
-                if(modeCryptoList) {
-                  return this.handleFixedValue(bigValue, rule.precision)
-                } else return this.handleFormattedValue(bigValue, '0.0-2')
-                
-              }
-            }
-          }
-        }
+    if (!value) {
+      return value;
+    }
+    const bigValue = isNaN(+value) ? new Big(0) : new Big(value);
+    if (bigValue.div(1).toNumber() === 0) {
+      return '0';
+    }
+    const numberRules = [
+      { range: [0, 0.1], precision: 8 },
+      { range: [0.1, 1.9], precision: 6 },
+      { range: [2, 9.9999], precision: 4 },
+      { range: [10, 9999.99], precision: 2 },
+      { range: [10000, Infinity], precision: 2 }
+    ];
+    for (const rule of numberRules) {
+      const [lower, upper] = rule.range;
+      if (bigValue.gte(lower) && (upper === Infinity || bigValue.lte(upper))) {
+        return modeCryptoList
+          ? this.handleFixedValue(bigValue, rule.precision)
+          : this.handleFormattedValue(bigValue, '0.0-2');
       }
     }
     return value;
