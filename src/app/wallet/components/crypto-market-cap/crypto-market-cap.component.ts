dcead782ecbc914d31ea637943d0f474ccc095ac
raniamorheg <morhegrania@gmai.com>
2023-05-24 11:28:00 +0100

    generate new component

---
+++ src/app/wallet/components/crypto-market-cap/crypto-market-cap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-market-cap',
+  templateUrl: './crypto-market-cap.component.html',
+  styleUrls: ['./crypto-market-cap.component.scss']
+})
+export class CryptoMarketCapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
