7af8d9a2329d62ebe0a244c0d7549bd12860eb52
hichri-louay <124356992+hichri-louay@users.noreply.github.com>
2023-06-13 11:59:06 +0100

    Launch campaign issues (#337)
    * wip : campaign workflow refactoring
    
    * change workflow of emit validators in components (campaign creation module)
    
    * remove logs
    
    * wip
    
    * wip

--- src/app/campaigns/components/draft-campaign-parametres/draft-campaign-parametres.component.ts
+++ src/app/campaigns/components/draft-campaign-parametres/draft-campaign-parametres.component.ts
@@ -3,28 +3,32 @@ import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
+  ElementRef,
   EventEmitter,
   Inject,
   Input,
   OnInit,
   Output,
   PLATFORM_ID,
+  Renderer2,
   SimpleChanges,
   ViewChild
 } from '@angular/core';
 import {
   AbstractControl,
+  FormControl,
   UntypedFormControl,
   UntypedFormGroup,
+  ValidatorFn,
   Validators
 } from '@angular/forms';
 import { Editor } from 'ngx-editor';
-import { debounceTime, takeUntil, tap } from 'rxjs/operators';
+import { debounceTime, take, takeUntil, tap } from 'rxjs/operators';
 import { DraftCampaignService } from '@campaigns/services/draft-campaign.service';
 import { Campaign } from '@app/models/campaign.model';
 import { TranslateService } from '@ngx-translate/core';
 import { arrayCountries } from '@app/config/atn.config';
-import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { DOCUMENT, formatDate, isPlatformBrowser } from '@angular/common';
 import {
   IDropdownSettings,
   MultiSelectComponent
@@ -78,6 +82,8 @@ export class DraftCampaignParametresComponent implements OnInit {
     @Inject(DOCUMENT) private document: Document,
     @Inject(PLATFORM_ID) private platformId: string,
     public translate: TranslateService,
+    private elRef: ElementRef,
+    private renderer: Renderer2,
     private refChangeDetect: ChangeDetectorRef
   ) {
     this.InterestList = [
@@ -108,11 +114,17 @@ export class DraftCampaignParametresComponent implements OnInit {
     this.form = new UntypedFormGroup(
       {
         startDate: new UntypedFormControl('', Validators.required),
-        endDate: new UntypedFormControl('', Validators.required),
+        endDate: new UntypedFormControl('', [Validators.required]),
         tags: new UntypedFormControl([], Validators.required)
       },
-      [DateValidator]
+      
+      
     );
+
+    // Wait for the form controls to have values
+this.form.controls.startDate.valueChanges.pipe(take(1)).subscribe(() => {
+  this.form.controls.endDate.setValidators([Validators.required, endDateGreaterThanStartDateValidator(this.form)]);
+});
 
     //tri
     // this.countriesListObj = this.countriesListObj.sort(function (
@@ -136,6 +148,8 @@ export class DraftCampaignParametresComponent implements OnInit {
   get endDate() {
     return this.form.get('endDate') as UntypedFormControl;
   }
+
+  
   ngAfterViewInit(): void {
     setTimeout(() => {
       if (this.draftData.targetedCountries.length === 0) {
@@ -152,45 +166,54 @@ export class DraftCampaignParametresComponent implements OnInit {
       //  elem.removeChild(elem.lastChild);
     }, 1000);
   }
-
+  
   ngOnInit(): void {
-    let day: any;
-    let month: any;
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth() + 1; //January is 0 so need to add 1 to make it 1!
-    var yyyy = today.getFullYear();
-    if (dd < 10) {
-      day = '0' + dd;
-    } else {
-      day = dd;
+    let today = new Date();
+    let formattedDate = this.formatDate(today);
+
+    let minStartDate = this.elRef.nativeElement.querySelector('#minStartDate');
+    let minEndDate = this.elRef.nativeElement.querySelector('#minEndDate');
+
+    if (minStartDate) {
+      this.renderer.setAttribute(minStartDate, 'min', formattedDate);
     }
-    if (mm < 10) {
-      month = '0' + mm;
-    } else {
-      month = mm;
+    if (minEndDate) {
+      this.renderer.setAttribute(minEndDate, 'min', formattedDate);
     }
-    let minDate = yyyy + '-' + month + '-' + day;
-    let minStartDate = this.document.getElementById('minStartDate');
-    let minEndDate = this.document.getElementById('minEndDate');
-    if (minStartDate) minStartDate.setAttribute('min', minDate);
-    if (minEndDate) minEndDate.setAttribute('min', minDate);
 
     this.saveForm();
     this.emitFormStatus();
-    /*
-    this.checkCountriesTags(this.form.value);
-*/
+  }
+ 
+  formatDate(date: Date): string {
+    let day = ('0' + date.getDate()).slice(-2); // adds leading zero if necessary
+    let month = ('0' + (date.getMonth() + 1)).slice(-2); // adds leading zero if necessary
+    let year = date.getFullYear();
+    return `${year}-${month}-${day}`;
   }
 
+
+  /** 
+   * 
+   *  SAVE END AND START DATE OF CAMPAIGN
+   * 
+   */
+
+
+  // SELECT DATE
+
+  changeDate(event:any, type: string) {
+    this.checkValidForm()
+  }
+
+
   ngOnChanges(changes: SimpleChanges) {
-    this.InterestList.forEach((tag: any) => {
-      if (this.draftData.tags.indexOf(tag.name) >= 0) {
-        tag.checked = true;
-        this.selectedTags.push(tag.name);
-      }
+    this.selectedTags = this.InterestList
+    .filter((tag:any) => this.draftData.tags.includes(tag.name))
+    .map((tag: any) => {
+      tag.checked = true;
+      return tag.name;
     });
-    this.maxTags = this.selectedTags.length;
     let today = new Date();
     let start: any;
     if (this.draftData.startDate) {
@@ -201,7 +224,6 @@ export class DraftCampaignParametresComponent implements OnInit {
       let end = new Date(this.draftData.endDate);
       this.draftData.endDate = end;
     }
-
     if (!this.draftData.startDate || !this.draftData.endDate) {
       let today = new Date();
       let end = new Date();
@@ -211,11 +233,7 @@ export class DraftCampaignParametresComponent implements OnInit {
       this.draftData.endDate = end;
     }
     if (changes.draftData && changes.draftData.currentValue.id) {
-      if (this.form.valid) {
-        this.validFormParam.emit(true);
-      } else {
-        this.validFormParam.emit(false);
-      }
+      this.checkValidForm()
       this.populateForm(this.draftData);
       if (
         this.draftData.startDate === '' ||
@@ -240,34 +258,36 @@ export class DraftCampaignParametresComponent implements OnInit {
     }
   }
 
+
+  /***
+   * 
+   *     FORMS UTILS
+   * 
+   * 
+   */
+
+  // SAVE FORM
+
   saveForm() {
     this.form.valueChanges
       .pipe(
         debounceTime(500),
         tap((values: any) => {
           if (this.draftData.id) {
-            /*
-            this.checkCountriesTags(values);
-*/
             this.service.autoSaveFormOnValueChanges({
               formData: values,
               id: this.id
             });
-          }
-          if (
-            this.form.valid &&
-            values.tags.length > 0 &&
-            this.selectedAttributes.length > 0
-          ) {
-            this.validFormParam.emit(true);
-          } else {
-            this.validFormParam.emit(false);
+           this.checkValidForm()
           }
         }),
         takeUntil(this.isDestroyed)
       )
       .subscribe();
   }
+
+
+  // EMIT FORM STATUS
 
   emitFormStatus() {
     this.service.saveStatus
@@ -276,6 +296,9 @@ export class DraftCampaignParametresComponent implements OnInit {
         this.saveFormStatus.emit(status);
       });
   }
+
+
+  // POPULATE FORM
 
   populateForm(data: Campaign) {
     this.form.patchValue(
@@ -300,6 +323,51 @@ export class DraftCampaignParametresComponent implements OnInit {
     //TODO populate tags and selected countries
   }
 
+  
+
+  
+
+  /**
+   * 
+   *            CHECK
+   *            VALID PARAMS
+   * 
+   * 
+   * 
+   */
+    checkValidForm() {
+      const isValidForm = this.form.get('endDate')?.errors === null && this.form.value.tags.length > 0 && this.selectedAttributes.length > 0;
+      this.validFormParam.emit(isValidForm)
+    }
+
+
+
+  /** 
+   * 
+   *  INTEREST LIST FUNCTIONS
+   * 
+   */
+
+
+  // SELECT INTEREST
+
+  toggleTag(index: number) {
+    const maxSelectedTags = 3;
+    const isTagCurrentlyChecked = this.InterestList[index].checked;
+    const countCheckedTags = this.form.get('tags')?.value.length;
+    if (!isTagCurrentlyChecked && countCheckedTags < maxSelectedTags) {
+      this.InterestList[index].checked = true;
+    } else if (isTagCurrentlyChecked) {
+      this.InterestList[index].checked = false;
+    }
+    this.selectedTags = this.InterestList.filter((tag:any) => tag.checked).map((tag:any) => tag.name);
+    this.saveTags(this.selectedTags);
+  }
+  
+
+
+  // SAVE INTERESTS
+
   saveTags(tags: string[]) {
     this.form.setValue({ ...this.form.value, tags: tags });
     if (this.draftData.id) {
@@ -307,82 +375,29 @@ export class DraftCampaignParametresComponent implements OnInit {
         formData: { ...this.form.value, tags },
         id: this.id
       });
-      if (
-        this.form.valid &&
-        tags.length > 0 &&
-        this.selectedAttributes.length > 0
-      ) {
-        this.validFormParam.emit(true);
-      } else {
-        this.validFormParam.emit(false);
-      }
+      this.checkValidForm();
     }
   }
 
-  //TODO: refactor this method to better solution or implement our own lib
-  checkCountriesTags(values: any) {
-    if (isPlatformBrowser(this.platformId)) {
-      if (values?.length === this.dropdownList.length) {
-        var elements = this.document.getElementsByClassName('selected-item');
-        var allCountriesElement: HTMLElement;
-        if (elements.length > 0) {
-          allCountriesElement = elements[0] as HTMLElement;
-          allCountriesElement.innerHTML = 'All countries';
-          for (let i = 1; i < elements.length; i++) {
-            let element = elements[i] as HTMLElement;
-            element.childNodes[0].nodeValue = values[i].item_text;
-            element.style.display = 'none';
-            if (element.innerText === '+230') {
-              element.style.display = 'none';
-            }
-          }
-        }
-        elements = this.document.getElementsByClassName('ng-star-inserted');
-        if (elements.length > 0) {
-          for (let i = 1; i < elements.length; i++) {
-            let element = elements[i] as HTMLElement;
-            if (element.innerText === '+230') {
-              element.style.display = 'none';
-            }
-          }
-        }
-      } else {
-        var elements = this.document.getElementsByClassName('selected-item');
-        if (elements.length > 0) {
-          for (let i = 0; i < elements.length; i++) {
-            let element = elements[i] as HTMLElement;
-            element.childNodes[0].nodeValue = !!values[i].item_text
-              ? values[i].item_text
-              : values[i];
-            element.style.display = 'block';
-          }
-        }
-      }
-    }
-  }
-  ngOnDestroy(): void {
-    this.isDestroyed.next('');
-    this.isDestroyed.unsubscribe();
+  // CHECK INTEREST FOR CSS RULES 
+
+  checkInterest(index: number) {
+    return this.selectedTags.some(element => element === this.InterestList[index].name);
   }
 
-  toggleTag(tagname: string, index: number) {
-    if (!this.InterestList[index].checked) {
-      if (this.maxTags < 3) {
-        this.InterestList[index].checked = true;
-        this.selectedTags.push(tagname);
-        this.maxTags++;
-      }
-    } else {
-      this.InterestList[index].checked = false;
-      this.maxTags--;
-    }
-    this.selectedTags = this.InterestList.filter((res: any) => res.checked).map(
-      (res: any) => res.name
-    );
-    this.saveTags(this.selectedTags);
-  }
 
-  saveCountries(countries: MultiSelectComponent) {
+
+
+   /** 
+   * 
+   *  COUNTRIES LIST FUNCTIONS
+   * 
+   */
+
+
+  // SELECT COUNTRIES
+
+  toggleCountries(countries: MultiSelectComponent) {
     const targetedCountries = countries.selectedItems.map((item: any) => {
       return { item_id: item.id, item_text: item.text };
     });
@@ -395,45 +410,79 @@ export class DraftCampaignParametresComponent implements OnInit {
     setTimeout(() => {
       this.checkCountriesTags(targetedCountries);
     }, 1000);
+    this.saveCountries(targetedCountries);
+  }
+
+
+  // PUT COUNTRIES SELECTED ON THE INPUT
+
+  checkCountriesTags(values: any) {
+    if (!isPlatformBrowser(this.platformId)) return;  
+    const selectedItemElements = Array.from(this.document.getElementsByClassName('selected-item') as HTMLCollectionOf<HTMLElement>);
+    const ngStarInsertedElements = Array.from(this.document.getElementsByClassName('ng-star-inserted') as HTMLCollectionOf<HTMLElement>);
+    if (values?.length === this.dropdownList.length) {
+        selectedItemElements[0].innerHTML = 'All countries';
+        selectedItemElements.slice(1).forEach((element, i) => {
+            element.childNodes[0].nodeValue = values[i].item_text;
+            element.style.display = (element.innerText === '+230') ? 'none' : 'none';
+        });
+        ngStarInsertedElements.slice(1).forEach(element => {
+            element.style.display = (element.innerText === '+230') ? 'none' : 'none';
+        });
+    } else {
+        selectedItemElements.forEach((element, i) => {
+            element.childNodes[0].nodeValue = !!values[i]?.item_text ? values[i].item_text : values[i];
+            element.style.display = 'block';
+        });
+    }
+  }
+
+  // SAVE COUNTRIES
+
+  saveCountries(targetedCountries: any) {
     if (this.draftData.id) {
       this.service.autoSaveFormOnValueChanges({
         formData: { ...this.form.value, targetedCountries },
         id: this.id
       });
-    }
-    if (
-      this.form.valid &&
-      this.form.value.tags.length > 0 &&
-      this.selectedAttributes.length > 0
-    ) {
-      this.validFormParam.emit(true);
-    } else {
-      this.validFormParam.emit(false);
+      this.checkValidForm();
     }
   }
+
+
+
+  // COMPONENT WILL DESTROY 
+
+  ngOnDestroy(): void {
+    this.isDestroyed.next('');
+    this.isDestroyed.unsubscribe();
+  }
+  
 }
 
-export function DateValidator(
-  control: AbstractControl
-): { [key: string]: boolean } | null {
-  let startDate = new Date(control.get('startDate')?.value);
-  let endDate = new Date(control.get('endDate')?.value);
 
-  let c = new Date();
-  if (startDate.setHours(0, 0, 0, 0) < c.setHours(0, 0, 0, 0)) {
-    return {
-      invaineFrom: true
-    };
-  }
-  if (endDate.setHours(0, 0, 0, 0) < c.setHours(0, 0, 0, 0)) {
-    return {
-      invaineTo: true
-    };
-  }
-  if (startDate > endDate) {
-    return {
-      dates: true
-    };
-  }
-  return {};
+
+
+/** 
+   * 
+   *  DATE VALIDATOR
+   * 
+   */
+function endDateGreaterThanStartDateValidator(formGroup: UntypedFormGroup): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const startDateControl = formGroup.get('startDate');
+    const endDateControl = formGroup.get('endDate');
+    if (startDateControl && endDateControl) {
+      const startDateValue = startDateControl.value;
+      const endDateValue = endDateControl.value;
+      if (startDateValue && endDateValue) {
+        const startDate = new Date(startDateValue);
+        const endDate = new Date(endDateValue);
+        if (endDate <= startDate) {
+          return { endDateLessThanStartDate: true };
+        }
+      }
+    }
+    return null;
+  };
 }
