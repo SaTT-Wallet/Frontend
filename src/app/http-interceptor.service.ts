4386094dc935a4f789356e2c6d1a70a11d15e963
skanderKhabou <skander.khabou@gmail.com>
2023-08-14 12:13:16 +0100

    resolve conflicts

--- src/app/http-interceptor.service.ts
+++ src/app/http-interceptor.service.ts
@@ -104,7 +104,10 @@ export class HttpInterceptorService implements HttpInterceptor {
     `${environment.API_URL}/profile/linkedin/ShareByActivity`,
     `${environment.API_URL}/profile/link/verify`,
     `${environment.API_URL}/profile/Tiktok/ProfilPrivacy`,
-    `${environment.API_URL}/profile/account`
+    `${environment.API_URL}/profile/account`,
+    `${environment.API_URL}/profile/check/threads-account`,
+    `${environment.API_URL}/profile/add/threads-account`,
+    `${environment.API_URL}/profile/remove/threads-account`
   ];
 
   constructor() {}
