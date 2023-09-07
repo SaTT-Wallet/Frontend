f3750053525c138dc5105c4596aea8708faff745
waelhanfi04 <wael@atayen.us>
2023-09-07 14:48:31 +0100

    fix redirection v1 V2

--- src/app/core/services/Auth/auth-store.service.ts
+++ src/app/core/services/Auth/auth-store.service.ts
@@ -31,7 +31,7 @@ export class AuthStoreService {
       tap((res) => {
         const walletVersion =
           this.tokenStorageService.getNewUserV2() === 'false' &&
-          !res.data.migrated
+          res.data.migrated === false
             ? 'v1'
             : 'v2';
 
