b3d1b247a9a4a92ce84d0d36afb83b5954be8def
skanderKhabou <skander.khabou@gmail.com>
2023-09-07 13:16:13 +0100

    design campaign and link notification add border to the notifs and handle responsivity in mobile centering the picture of notif campaign and link campaign in mobile devices

--- src/app/notifications/notification.component.ts
+++ src/app/notifications/notification.component.ts
@@ -540,10 +540,20 @@ export class NotificationComponent implements OnInit {
   hideNotification() {
     this.showNotification = false;
   }
+  getStorageInformation() {
+    return window.localStorage.getItem('phishing');
+  }
+
+  isPhishing() {
+    if(this.getStorageInformation()){
+      return 'isPhish';
+    }
+    return;
+  }
+
   ngOnInit(): void {
     this.getAllNotifications();
-    this.getNotificationsDecision();
-    
+    this.getNotificationsDecision(); 
   }
   seeNotification() {
     this.NotificationService.notificationSeen()
@@ -862,6 +872,8 @@ closeModal(content: any) {
       this.showSpinner = true;
     }
   }
+
+ 
 
   getCampaignCover(cover: string) {
     return cover.replace('ipfs:', '');
